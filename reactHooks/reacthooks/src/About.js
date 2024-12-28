35874c2341c0d5f7d7e8e9a40631b8188eeca745
instructorLikith <instructorlikith@gmail.com>
2024-12-27 21:37:21 -0800

    react hooks contn 27-12-24

---
+++ reactHooks/reacthooks/src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1> i am about page</h1>
+    </>
+  );
+}
+
+export default About;
--- reactHooks/reacthooks/src/App.js
+++ reactHooks/reacthooks/src/App.js
@@ -1,5 +1,5 @@
-import React,{useState, useEffect}  from "react";
-import  {data}   from './data';
+import React, { useState, useEffect } from "react";
+import { data } from "./data";
 
 // Props , State & Routing imp in react
 // State -useState
@@ -44,42 +44,161 @@ import  {data}   from './data';
 //   );
 // }
 
-
 //useEffect hook
+// conditional rendering
+// function App(){
 
-function App(){
+//   // declaring a variable and using useState
+//   const [count, setCount] = useState(0);
 
-  // declaring a variable and using useState
-  const [count, setCount] = useState(0);
+// // useEffect takes a function as input
+//   useEffect(() => {
+//     console.log('use Effect called')
+//     if(count > 5){
+//       setCount(0);
+//     }
+//   },[count])
 
+//   useEffect(() => {
+//     console.log('hello')
+//   },[])
 
-// useEffect takes a function as input
-  useEffect(() => {
-    console.log('use Effect called')
-    if(count > 5){
-      setCount(0);
-    }
-  },[count])
+//   function increaseValue(){
+//     setCount(count + 1);
+//   }
 
-  useEffect(() => {
-    console.log('hello')
-  },[])
+//   return (
+//     <>
+//       <h1>click counter</h1>
+//       <h2>{count}</h2>
+//       <button type="button" onClick={increaseValue}>increase value</button>
+//     </>
+//   );
+// }
 
-  function increaseValue(){
-    setCount(count + 1);
-  }
+// https://api.github.com/users
+// function App() {
+//   const url = "https://api.github.com/users";
+//   const [gusers, setGusers] = useState([]);
 
+//   async function getData() {
+//     const response = await fetch(url);
+//     const users = await response.json();
+//     setGusers(users);
+//   }
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   return (
+//     <>
+//     {gusers.map((user)=> {
+//       return <li>{user.login}</li>
+//     })}
+//     </>
+//   );
+// }
+
+//multiple returns
+// function App() {
+//   //conditional rendering
+//   const [loading, setLoading] = useState(false);
+
+//   if (loading) {
+//     return "data loading ....";
+//   }
+
+//   return (
+//     <>
+//       <h1>Multiple Return</h1>
+//     </>
+//   );
+// }
+
+// short circuit evaluation
+// function App(){
+//   //short-circuit evaluation
+//   const [text, setText] = useState('abc');
+//   const firstValue = text || "hello world";
+//   const secondValue = text && "hello world";
+//   return(
+//     <>
+//       <h1>1st : {firstValue}</h1>
+//       <h1>{secondValue}</h1>
+//     </>
+//   )
+// }
+
+// ternary operator
+// function App() {
+//   // ternary operator
+//   // statement ? true : false
+
+//   const [text, setText] = useState("abc");
+//   function handleClick() {
+//     setText(!text);
+//   }
+
+//   return (
+//     <>
+//       {text ? <h1>Good</h1> : <h1>Bad</h1>}
+//       <button onClick={handleClick}>click me</button>
+//     </>
+//   );
+// }
+
+//forms
+// function App() {
+
+//   function handleSubmit(e){
+//     e.preventDefault();
+//     console.log("hello world")
+//   }
+
+//   const [name, setName] = useState('');
+//   const [age, setAge] = useState('');
+
+//   function handleNameChange(e){
+//     setName(e.target.value)
+//   }
+
+//   function handleAgeChange(e){
+//     setAge(e.target.value)
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="name">Name</label>
+//         <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/>
+
+//         <label htmlFor="age">Age</label>
+//         <input type="number" id="age" name="age" value={age} onChange={handleAgeChange}/>
+//         <button type="submit">Submit</button>
+//       </form>
+//     </>
+//   );
+// }
+
+import Home from "./Home";
+import About from "./About"
+import Contact from "./Contact"
+import Error from "./Error"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// react router
+//npm install react-router-dom
+function App() {
   return (
-    <>
-      <h1>click counter</h1>
-      <h2>{count}</h2>
-      <button type="button" onClick={increaseValue}>increase value</button>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
-
-export default App; 
-
-
-
+export default App;
