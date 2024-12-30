36ef91a978a19b93a29a861d4c0d5044cbb82e7d
instructorLikith <instructorlikith@gmail.com>
2024-12-30 07:51:23 -0800

    python-30-12-24

---
+++ pythondemo/1.py
@@ -0,0 +1,86 @@
+# What is Python - 
+# 	High level, interpreted programming language
+# * Web Development (e.g django, Flask)
+# * Data Science and Machine Learning (e.g pandas, TensorFlow)
+# * Automation (e.g scripting tasks)
+# * Software development and many more
+
+# Why is it so popular -
+# 	* Easy to learn
+# 	* community support
+# 	* Cross-platform 
+# 	* Versatile 
+
+# Python - SetUp 
+# 	https://www.python.org/
+# 	go to download and download latest version
+# 	select add to path and install now
+
+# Python - IDE (Integrated Development Environment)
+# 	* PyCharm (community edition - https://www.jetbrains.com/pycharm/editions/)
+# 	* VS Code
+# 	* jupyter Notebook
+
+# verify the installation
+# 	"python --version" in cmd
+
+# Key Features of Python
+# 	* Simple syntax 
+# 	* Interpreted - executed line by line
+# 	* Dynamically Typed - No Need to declare the variable types explicitly
+# 	* Object-Oriented - Supports OOP (Object oriented programming) like classes and objects
+# 	* Rich Standard Library - it has built in modules
+# 1st code
+# open IDE -> Create a python file 1.py -> print("Hello") -->in terminal run "python 1.py"
+
+print("Hello, world!!")
+
+# variables & Data types in python
+x = 5
+# print(type(x))
+X = 6
+y = "Hello"
+is_true = True
+#print(type(y))
+
+# type conversion
+x = "10"
+print(x)
+print(type(x))
+a = float(x)
+print(a)
+print(type(a))
+b = int(a)
+print(b)
+print(type(b))
+
+# Airthmetic Operation
+# + addition
+# - sub
+# * mul
+# / div
+# // floor div
+# % modulus
+# ** Exponentiation
+
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# assigning values to multiple variables
+x,y,z = 11,12,13
+print(x)
+print(y)
+print(z)
+
+# variable reassigment
+g = 5
+print(g)
+g = 10
+print(g)
