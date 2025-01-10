76356942b96bbac2351ae74caae6cdba47d4bed1
instructorLikith <instructorlikith@gmail.com>
2025-01-10 08:09:53 -0800

    django 3 - 10-1-25

--- djangodemo/myproject/myapp/views.py
+++ djangodemo/myproject/myapp/views.py
@@ -1,33 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    features = Feature.objects.all()
+    return render(request,'index.html', {'features':features})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
     
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast4'
-    feature1.details = 'Our Application is very Fast'
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    feature2 = Feature()
-    feature2.id = 0
-    feature2.name = 'Fast1'
-    feature2.details = 'Our Application is very Fast'
+        user = auth.authenticate(username = username, password= password)
 
-    feature3 = Feature()
-    feature3.id = 0
-    feature3.name = 'Fast2'
-    feature3.details = 'Our Application is very Fast'
-
-    feature4 = Feature()
-    feature4.id = 0
-    feature4.name = 'Fast3'
-    feature4.details = 'Our Application is very Fast'
-
-    return render(request,'index.html', {'feature1':feature1, 'feature2':feature2, 'feature3':feature3, 'feature4':feature4})
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 def counter(request):
     text = request.POST['text']
     number_of_words = len(text.split())
     return render(request, 'counter.html', {'number': number_of_words})
+
